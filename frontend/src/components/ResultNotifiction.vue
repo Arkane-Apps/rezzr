60df74a02683737344aa3dc18ed25241e8ea40c1
Steven Arkwright <arkane@arkane.app>
2023-04-23 13:08:32 -0400

    Notification sketch

---
+++ frontend/src/components/ResultNotifiction.vue
@@ -0,0 +1,23 @@
+<script>
+import { appState } from '../state'
+
+export default {
+    data() {
+        return {
+            appState,
+            props: ['notificationClass'],
+        }
+    },
+    methods: {
+        closeNotification() {
+            appState.showNotification = false
+        }
+    }
+}
+</script>
+<template>
+    <div class="notification" :class="props.notificationClass" v-if="appState.showNotification">
+        <button class="delete" @click="closeNotification"></button>
+        <slot />
+    </div>
+</template>
--- frontend/src/state.ts
+++ frontend/src/state.ts
@@ -2,5 +2,6 @@ import { reactive } from 'vue'
 
 export const appState = reactive({
   showInventoryCreateModal: false,
-  showReservationCreateModal: false
+  showReservationCreateModal: false,
+  showNotification: false
 })
